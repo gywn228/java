264ec97b7809ea3e91ecb879e7335964a1e5d04a
gywn228 <gywn228@naver.com>
2024-07-22 15:14:41 +0900

    Add files via upload

--- Day02/casting/qiuz.js
+++ Day02/casting/qiuz.js
@@ -5,19 +5,19 @@
 // window.console.log(`당신의 나이는 ${age} 이군요! `);
 
 
-// const first = window.prompt("첫번째 숫자 입력");
-// const second = window.prompt("두번째 숫자 입력");
-// const numFirst = Number(first);
-// const numSecond = Number(second);
+const first = window.prompt("첫번째 숫자 입력");
+const second = window.prompt("두번째 숫자 입력");
+const numFirst = Number(first);
+const numSecond = Number(second);
 
-//console.log(`두 수의 합:${numFirst + numSecond}, 차:${numFirst - numSecond}, 곱:차:${numFirst * numSecond}`)
+console.log(`두 수의 합:${numFirst + numSecond}, 차:${numFirst - numSecond}, 곱:차:${numFirst * numSecond}`);
 
 
 
 // const side = Number(prompt("한 변의 길이"));
 // console.log(`정사각형의 넓이는 ${side * side} 입니다`);
 
-window.prompt("아아(1500)2개, 라떼(2500)2개는 얼마입니까")
-const coffee1 = 1500 * 2
-const coffee2 = 2500 * 2
-window.console.log (`총 가격은 ${coffee1 + coffee2}원 입니다`)
+// const ame = window.prompt("아아 주문 갯수를 입력하세요");
+// const latte = window.prompt("라떼 주문 갯수를 입력하세요");
+// const coffee = 1500 * Number(ame) + 2500 * Number(latte);
+// window.console.log (`총 가격은 ${coffee}원 입니다`);
