0069292aa60a3ee9bf4db1e591788a63e3b7b7c1
gywn228 <gywn228@naver.com>
2024-07-22 14:50:28 +0900

    Add files via upload

---
+++ Day02/casting/qiuz.js
@@ -0,0 +1,23 @@
+// const year = window.prompt("당신은 몇년생인가요?"); //year 문자
+// const age = 2024 - Number(year)+1;
+
+
+// window.console.log(`당신의 나이는 ${age} 이군요! `);
+
+
+// const first = window.prompt("첫번째 숫자 입력");
+// const second = window.prompt("두번째 숫자 입력");
+// const numFirst = Number(first);
+// const numSecond = Number(second);
+
+//console.log(`두 수의 합:${numFirst + numSecond}, 차:${numFirst - numSecond}, 곱:차:${numFirst * numSecond}`)
+
+
+
+// const side = Number(prompt("한 변의 길이"));
+// console.log(`정사각형의 넓이는 ${side * side} 입니다`);
+
+window.prompt("아아(1500)2개, 라떼(2500)2개는 얼마입니까")
+const coffee1 = 1500 * 2
+const coffee2 = 2500 * 2
+window.console.log (`총 가격은 ${coffee1 + coffee2}원 입니다`)
---
+++ Day02/review/review.js
@@ -0,0 +1,10 @@
+// window.console.log("주말에 친구들 만났다")
+// window.alert("오늘 수박주스 마셨다")
+
+
+const mbti = "estp";
+
+window.console.log = (`나의 mbti는 ${mbti} 입니다`);
+
+const feeling = window.prompt("기분이 어떤가요?");
+window.console.log(`기분이 ${feeling}하시군요`);
