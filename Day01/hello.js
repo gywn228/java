9026c03869cbe54eadee8a16b8c0688ddcdfa7d5
gywn228 <gywn228@naver.com>
2024-07-24 14:46:29 +0900

    Add files via upload

---
+++ Day01/hello.js
@@ -0,0 +1,11 @@
+//window.alert("오늘 자바스크립트 배우는 날");
+
+//window.alert("오늘 점심은 연어장덮밥");
+
+//window.console.log("주말에는 친구들 만날거임");
+
+//window.console.log("주말엔 친구들 만난당");
+//window.console.log("오늘 저녁 메뉴는 피자");
+//window.console.log("재밌당");
+
+
---
+++ Day01/memory/index.js
@@ -0,0 +1,26 @@
+const name = "김효주";
+const coffee = "아이스아메리카노";
+
+// window.console.log(name);
+// window.console.log(coffee);
+// window.console.log("제 이름은 name 좋아하는 커피는 coffee 입니다.");
+// window.console.log(`제 이름은 ${name} 좋아하는 커피는 ${coffee} 입니다.`);
+
+const mbti = "estp";
+const weekend = "친구들 만나고";
+const youtube = "짱구";
+
+// window.console.log(mbti);
+// window.console.log(weekend);
+// window.console.log(youtube);
+
+// window.console.log(
+//   `제 mbti는 ${mbti}이구요 주말엔 ${weekend}요 유튜브는 ${youtube}를 볼거에요!`
+// );
+
+const phone = window.prompt("당신의 핸드폰 기종은?");
+const pc = window.prompt("당신의 pc기종은?");
+
+window.console.log(
+  `당신의 핸드폰 기종은 ${phone}이고,당신의 PC는 ${pc} 입니다`
+);
